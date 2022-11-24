0f1e906abbb7ce92e3ffba1e83d5fe200770f65c
jalallinux <smjjalalzadeh93@gmail.com>
2022-11-25 00:45:21 +0330

    refactor t support laravel 7.*, 8.*, 9.*

--- src/ShinobiServiceProvider.php
+++ src/ShinobiServiceProvider.php
@@ -2,24 +2,42 @@
 
 namespace JalalLinuX\Shinobi;
 
-use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
-use JalalLinuX\Shinobi\Commands\ShinobiCommand;
-use \JalalLinuX\Shinobi\Facades\Shinobi as ShinobiFacade;
+use Illuminate\Support\ServiceProvider;
+use JalalLinuX\Shinobi\Facades\Shinobi as ShinobiFacade;
 
-class ShinobiServiceProvider extends PackageServiceProvider
+class ShinobiServiceProvider extends ServiceProvider
 {
     public function registeringPackage()
     {
         $this->app->singleton(ShinobiFacade::class, Shinobi::class);
     }
 
-    public function configurePackage(Package $package): void
+    public function register()
     {
-        $package
-            ->name('shinobi-cctv')
-            ->hasConfigFile('shinobi')
-            ->hasViews()
-            ->hasCommand(ShinobiCommand::class);
+        $this->mergeConfigFrom(__DIR__.'/../config/shinobi.php', 'shinobi');
+    }
+
+    public function boot()
+    {
+        $this->registerCommands();
+        $this->registerPublishing();
+    }
+
+    protected function registerPublishing()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/shinobi.php' => config_path('shinobi.php'),
+            ]);
+        }
+    }
+
+    protected function registerCommands()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                Commands\ShinobiCommand::class,
+            ]);
+        }
     }
 }
